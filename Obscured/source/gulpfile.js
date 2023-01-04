9099ced8d0c81e5c927e6829bc393c44471d2ed7
Hendro Smit <smit.gamedev@gmail.com>
2023-01-04 19:20:38 +0200

    Dev environment change

--- Obscured/source/gulpfile.js
+++ Obscured/source/gulpfile.js
@@ -18,21 +18,10 @@ gulp.task('html', () => {
     }))
     .pipe(gulp.dest(PATHS.dist))
 })
- 
-gulp.task('preview', () => {
-  return gulp
-  .src(PATHS.dev)
-    .pipe(pug({
-      pretty: true,
-      locals: require(PATHS.translation)
-    }))
-    .pipe(gulp.dest(PATHS.preview))
-})
+
 
 gulp.task('build',  gulp.series(['html']))
 
-gulp.task('dev',  gulp.series(['preview']))
-
-gulp.task('watch', gulp.series(['preview'], () => {
-  gulp.watch(['./app/**/*.pug','./app/**/*.js'], gulp.series(['preview']))
+gulp.task('watch', gulp.series(['html'], () => {
+  gulp.watch(['./app/**/*.pug','./app/**/*.js'], gulp.series(['html']))
 }))
